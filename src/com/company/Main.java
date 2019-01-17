66c29fac9d6a0d48f3b98fc0aed7a0b1a9363afe
Brian B <brianb@MacBook-Pro-4.local>
2019-01-17 15:47:30 -0500

    Adding for-loop and total variable

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int total = 0;
+
+        for(int i = 0; i < 1000; i++){
+
+           // if(i % 5 == 0 || i % 3 == 0){
+           //     total += i;
+            }
+        }
+
+       // System.out.println(total);
+    }
+}
